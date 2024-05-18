d91dda406b5ff1b02189aa311a194db9766c25a1
Imtiaz <im3azchowdhury@gmail.com>
2024-05-18 11:55:02 +0600

    dev and prod setup

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 const app: Application = express();
-const port = 3000;
 
 // parser
 app.use(express.json());
@@ -11,10 +10,6 @@ app.use(cors());
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
 
 console.log(process.cwd());
