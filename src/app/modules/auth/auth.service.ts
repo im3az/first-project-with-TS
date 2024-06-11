c9b26ec1679469acf0b6b0c6ac76a4da614b96cc
Imtiaz <im3azchowdhury@gmail.com>
2024-06-11 09:55:45 +0600

    auth service validation added

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,38 @@
+import httpStatus from 'http-status';
+import AppError from '../../error/AppError';
 import { TLoginUser } from './auth.interface';
+import { User } from '../user/user.model';
+import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: TLoginUser) => {
-    
+  // checking if the user is exist
+  const user = await User.findOne({ id: payload.id });
+
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found!');
+  }
+
+  // checking if the user is already deleted
+
+  const isDeleted = user?.isDeleted;
+
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'The user is deleted!');
+  }
+
+  // checking if the user is blocked
+
+  const userStatus = user?.status;
+
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'The user is blocked!');
+  }
+
+  //checking if the password is correct
+  const isPasswordMatched = await bcrypt.compare(
+    payload?.password,
+    user?.password,
+  );
 };
 
 export const AuthServices = {
